91e161bedfd943ffc865f7e0cceecd18b5219ed6
Minh Luan <phamminhluan@fabercompany.co.jp>
2024-07-03 14:35:45 +0700

    update link

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -514,13 +514,13 @@
           <div class="flex flex-wrap">
             <div class="w-full lg:w-4/12 px-4">
               <h5 class="text-xl font-semibold pb-4 text-center">Login Page</h5>
-              <Link to="/auth/login">
+              <a href="https://minhluanpham1309.github.io/TakedaBlog/">
                 <div
                   class="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150"
                 >
                   <img alt="..." class="align-middle border-none max-w-full h-auto rounded-lg" src={login} />
                 </div>
-              </Link>
+              </a>
             </div>
 
             <div class="w-full lg:w-4/12 px-4">
